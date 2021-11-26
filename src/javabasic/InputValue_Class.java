2d4997ae76ce9914ca4281f58e1a76a7f0b222fb
prashant babar <prashantb@mkcl.org>
2021-11-26 18:14:43 +0530

    This Is my THIRD Comment.

--- src/javabasic/InputValue_Class.java
+++ src/javabasic/InputValue_Class.java
@@ -1,10 +1,11 @@
 package javabasic;
 import java.util.Scanner;
 
+
 public class InputValue_Class {
     public static void main(String[] args) {
         System.out.println("prashant");
-        Scanner pb =new Scanner(System.in); //Object created for Scanner Class
+        Scanner pb = new Scanner(System.in); //Object created for Scanner Class
         System.out.println("enter number one is");
         int a = pb.nextInt(); //value entered from keyboard,it is saved in variable "a"     next.in is a method from Scanner Class
         System.out.println("enter number two is");
